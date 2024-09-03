ff6bc436cced0a98bb0308d9edd460d408590181
ankitraj20616 <ankitrajthakur803@gmail.com>
2024-09-03 23:13:22 +0530

    Dev Dependencies

--- sensor_sketch_sep3a/sensor_sketch_sep3a.ino
+++ sensor_sketch_sep3a/sensor_sketch_sep3a.ino
@@ -1,6 +1,7 @@
 int sensorInputPin = A1;
 int Button=8;
 int sensorData;
+int sensorDigitalData;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -12,9 +13,9 @@ void loop() {
   // put your main code here, to run repeatedly:
   if(digitalRead(Button) == HIGH){
     sensorData = analogRead(sensorInputPin);
-    
-    Serial.println(sensorData);
-    delay(1000);
+    sensorDigitalData = map(sensorData, 0, 1023, 255, 0);
+    Serial.println(sensorDigitalData);
+    delay(200);
   }
   
 
