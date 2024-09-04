c704b7ca4001b2ff563db0c6c54fa95717e767d7
Suman9038 <sumanjhad903878@gmail.com>
2024-09-04 18:11:40 +0530

    Script to check weather conditions

--- Weather_Forecast.py
+++ Weather_Forecast.py
@@ -1,5 +1,7 @@
 import requests
 from GPS import FindCurrentLocation
+
+
 class  FindCurrentWeather : 
 
     def detectWeather(self):
@@ -11,7 +13,32 @@ class  FindCurrentWeather :
         api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{state}&APPID={api_key}'
         
         api_response = requests.get(api_url)
-        print(api_response.text)
+        
+        return api_response.json()
+        
+    def detectRain(self):
+        weather_data = self.detectWeather()
+
+        temperature = weather_data['main']['temp'] - 273.15 # Converted Into Celcius 
+        humidity = weather_data['main']['humidity']
+        weather_description = weather_data['weather'][0]['description']
+
+        if temperature > 30:
+            print("Temperature in the surrounding is very high, Water the plant")
+            return
+
+        elif humidity < 30:
+            print("Humidity is very low, Water the plant")
+            return
+        
+        elif 'rain' not in weather_description or 'thunderstorm' not in weather_description:
+            print("Rain is not going to happen now, Water the plant")
+            return
+        else:
+            print("You don't have to Water the plant now.")
+        
+        
+
     
 
     
@@ -19,4 +46,4 @@ class  FindCurrentWeather :
 
 if __name__ == "__main__":
     weatherDetector = FindCurrentWeather()
-    weatherDetector.detectWeather()
+    weatherDetector.detectRain()
